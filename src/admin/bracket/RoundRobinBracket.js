371763839f4a6d1af6d320c6103eeea70659b31b
Божидар Димов <bojko958@abv.bg>
2018-06-27 20:01:35 +0300

    round-robin ui

--- src/admin/bracket/RoundRobinBracket.js
+++ src/admin/bracket/RoundRobinBracket.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { TeamLabel } from './TeamLabel';
+import { Score } from './Score';
+import { MatchScore2 } from './MatchScore';
 
 export class RoundRobinBracket extends React.Component {
   render() {
@@ -22,21 +25,47 @@ export class BracketGroup extends React.Component {
     return String.fromCharCode("А".charCodeAt(0) + this.props.group.group);
   }
 
-  get_match(i, j) {
-    let team1Id = this.props.group.teams[i];
-    let team2Id = this.props.group.teams[j];
-    let match = this.props.group.matches.find(match => match.team1Id === team1Id && match.team2Id === team2Id);
+  removeTeam(pos) {
+    // get(`/matches/${this.props.match.id}/removeTeam?pos=${pos}`)
+    //   .then(() => this.props.refresh());
+  }
 
-    if (team1Id == team2Id)
-      return <div>x</div>;
-    else if (match)
-      return <div>има среща</div>;
-    else return <div>няма среща</div>;
+  isReversed(team1Id, team2Id) {
+    return this.props.group.matches.find(e => e.team1Id == team2Id && e.team2Id == team1Id);
+  }
+
+  getMatch(t1, t2) {
+    console.log(t1, t2);
+    let match = this.props.group.matches.find(e => e.team1Id == t1.team1Id && e.team2Id == t2.team2Id);
+    let reversed = this.isReversed(t1.team1Id, t2.team2Id);
+
+    if (match)
+      return match;
+    else if (reversed)
+      return reversed;
+    else return {
+      groupId: this.props.group.id,
+      schemeId: this.props.group.schemeId,
+      team1Id: t1.team1Id,
+      team2Id: t2.team2Id,
+      team1: t1.User,
+      team2: t2.User,
+      sets: []
+    };
+  }
+
+  getSets(t1, t2) {
+    let match = this.getMatch(t1, t2);
+
+    if (match)
+      return match.sets;
+    else
+      return [];
   }
 
   render() {
     return (
-      <table>
+      <table className="round-robin-table">
         <thead>
           <tr>
             <th>{`Група "${this.get_header()}"`}</th>
@@ -46,10 +75,23 @@ export class BracketGroup extends React.Component {
         <tbody>
           {this.props.group.teams.map((t1, i) => (
             <tr key={i}>
-              <td>
-                {`${t1.order}. ${t1.User.fullname}`}
+              <td className="team-label">
+                <span>{`${t1.order}. `}</span>
+                <TeamLabel team={t1.User} />
               </td>
-              {this.props.group.teams.map((t2, j) => <td key={j}>{this.get_match(i, j)}</td>)}
+              {this.props.group.teams.map((t2, j) => (
+                <td key={j}>
+                  {i == j ? <span>x</span> :
+                    <React.Fragment>
+                      <Score
+                        sets={this.getSets(t1, t2)}
+                        reversed={this.isReversed(t1.team1Id, t2.team2Id)}
+                      />
+                      <MatchScore2 match={this.getMatch(t1, t2)} refresh={this.props.refresh} />
+                    </React.Fragment>
+                  }
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
--- src/admin/bracket/Score.js
+++ src/admin/bracket/Score.js
@@ -4,13 +4,24 @@ export class Score extends React.Component {
   render() {
     if (this.props.sets.length > 0)
       return <div className="button-group score">
-        {this.props.sets.map((set, i) => (
-          <React.Fragment key={i}>
-            <span>{(" " + set.team1).split('(')[0]}</span>
-            <span>{(set.team2 + "").split('(')[0]}</span>
-            <sup>{set.tiebreaker}</sup>
-          </React.Fragment>
-        ))}
+        {this.props.sets.map((set, i) => {
+          if (this.props.reversed)
+            return (
+              <React.Fragment key={i}>
+                <span>{(" " + set.team2).split('(')[0]}</span>
+                <span>{(set.team1 + "").split('(')[0]}</span>
+                <sup>{set.tiebreaker}</sup>
+              </React.Fragment>
+            );
+          else
+            return (
+              <React.Fragment key={i}>
+                <span>{(" " + set.team1).split('(')[0]}</span>
+                <span>{(set.team2 + "").split('(')[0]}</span>
+                <sup>{set.tiebreaker}</sup>
+              </React.Fragment>
+            );
+        })}
       </div>
     else return null;
   }
