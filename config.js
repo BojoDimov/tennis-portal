3d1c457491f027c859fa9d1fffddfa3c85970849
Божидар Димов <bojko958@abv.bg>
2018-07-03 10:54:59 +0300

    extracted sequelize configuration to file

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "production": {
+    database: "tennis-portal-db",
+    username: "postgres",
+    password: "12345678",
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    operatorsAliases: false
+  },
+  "development": {
+    database: "tennis-portal-db",
+    username: "postgres",
+    password: "12345678",
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    operatorsAliases: false
+  }
+}
