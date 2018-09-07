9f6ce4ab937aa1321c591c6f600cf3365a65a9a7
Божидар Димов <bojko958@abv.bg>
2018-09-07 15:46:13 +0300

    logging of validation errors

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -4,7 +4,18 @@ module.exports = (err, req, res, next) => {
   if (err.name === 'SequelizeValidationError') {
     const result = {}
     err.errors.forEach(e => result[e.path] = e.message);
-    res.status(422).send(result);
+
+    return Logs
+      .create({
+        ip: req.ip,
+        path: req.path,
+        method: req.method,
+        body: null,
+        params: JSON.stringify(req.params),
+        query: JSON.stringify(req.query),
+        error: JSON.stringify(result)
+      })
+      .then(e => res.status(422).send(result));
   }
   else if (err.name === 'SequelizeUniqueConstraintError') {
     const result = {}
