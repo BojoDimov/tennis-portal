5bcc13a52a16f61824f63e2f24d98a7472411c9b
Божидар Димов <bojko958@abv.bg>
2018-09-07 15:49:10 +0300

    logging of unique errors

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -21,7 +21,17 @@ module.exports = (err, req, res, next) => {
     const result = {}
     const keys = Object.keys(err.fields);
     keys.forEach(key => result[key + '_uq'] = err.fields[key]);
-    res.status(422).send(result);
+    return Logs
+      .create({
+        ip: req.ip,
+        path: req.path,
+        method: req.method,
+        body: null,
+        params: JSON.stringify(req.params),
+        query: JSON.stringify(req.query),
+        error: JSON.stringify(result)
+      })
+      .then(e => res.status(422).send(result));
   }
   else if (err.name === 'DomainActionError') {
     res.status(422).send(err.error);
