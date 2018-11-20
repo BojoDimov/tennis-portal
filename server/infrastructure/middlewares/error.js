b5c8d0d3f3842b77d303e5ae03c09b3924684b74
Божидар Димов <bojko958@abv.bg>
2018-11-20 18:00:42 +0200

    made error middleware to return proper object on error

--- server/infrastructure/middlewares/error.js
+++ server/infrastructure/middlewares/error.js
@@ -1,6 +1,6 @@
 const { Logs } = require('../../db');
 
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res) => {
   if (err.name === 'SequelizeValidationError') {
     const result = {}
     err.errors.forEach(e => result[e.path] = e.message);
@@ -48,5 +48,5 @@ module.exports = (err, req, res, next) => {
       query: JSON.stringify(req.query),
       error: JSON.stringify(err)
     })
-    .then(() => res.status(500).end());
+    .then(() => res.status(500).send({ message: 'Internal server error' }));
 }
