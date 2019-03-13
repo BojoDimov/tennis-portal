cc6e586c3463478df506e5eae39fd5ae1f4fedbf
Божидар Димов <bojko958@abv.bg>
2019-03-13 21:30:15 +0200

    build 3.0.1.1

---
+++ build/precache-manifest.f9aa11562128d4a5211693bc0829ed33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6dc48d02dc405111530b",
+    "url": "/static/js/main.6dc48d02.chunk.js"
+  },
+  {
+    "revision": "8bc2366e12b16e493ba6",
+    "url": "/static/js/1.8bc2366e.chunk.js"
+  },
+  {
+    "revision": "6dc48d02dc405111530b",
+    "url": "/static/css/main.eb46ad82.chunk.css"
+  },
+  {
+    "revision": "9983d8274eb086446cf7264fe77b5c73",
+    "url": "/index.html"
+  }
+];
