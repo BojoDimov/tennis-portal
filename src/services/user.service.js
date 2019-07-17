c559888ae134ac25d58220ec69f9154ba836a069
Божидар Димов <bojko958@abv.bg>
2019-07-17 19:37:14 +0300

    fixed the making of 2691248592 authData requests

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,22 +9,31 @@ class UserService {
     const { Provider, Consumer } = React.createContext(ApplicationMode.GUEST);
     this.SetApplicationMode = Provider;
     this.WithApplicationMode = Consumer;
+    this.cached = false;
+    this.user = null;
   }
 
   async getAuthenticatedUser() {
+    if (this.cached)
+      return this.user;
+
     return QueryService.get('/login/authData')
       .then(({ data }) => {
+        this.cached = true;
+        this.user = data;
         return data;
       });
   }
 
   login(data) {
     localStorage.setItem('token', data.token);
+    this.cached = false;
     dispatchEvent('login');
   }
 
   logout() {
     localStorage.removeItem('token');
+    this.cached = false;
     dispatchEvent('logout');
   }
 }
